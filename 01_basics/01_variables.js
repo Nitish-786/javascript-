9b8ce95731bc5e516d8346a6991d3021dedaa8fd
Nitish kumar <nitishknitish777@gmail.com>
2024-09-12 13:04:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "nitishknitish@gmail.accountEmail"
+var accountPassword = "12345"
+accountCity = "patna"
+let accountState;
+//accountId = 2// not allowed
+
+accountEmail = "rachi@gmail.com"
+accountPassword = "8529"
+accountCity = "Rachi"
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+/*
+perefer not to use var
+because of issue in block scope and function scope 
+*/
