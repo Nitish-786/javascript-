11fb013ccd00bc54bf785b2fa0063c3a1103df38
Nitish kumar <nitishknitish777@gmail.com>
2024-09-18 08:00:27 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3 ) we are using node js not browser
+console.log( 3 
+    + 3)//code readiability should be high 
+    console.log("nitish")
+    
+    let name = ("nitish")
+    let age = 18
+    let isLoggedIn = false
+    let state = null
+
+    //number => 2 to power 53
+    //bright 
+    //string
+    //boolean
+    //null => standalone value 
+    //undefined=>
+    //symbol=>
+    //object
+    console.log(typeof "nitish"); //string
+    console.log(typeof"null");//object
+    
