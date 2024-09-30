72ac5b33587980b15e538c65fc9a80f4d0f8b39b
Nitish kumar <nitishknitish777@gmail.com>
2024-09-30 18:02:17 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+//singleton
+// object literals
+//object literals 
+
+const Jsuser = {
+    name:"nitish",
+    "full name": "Nitish kumar",
+    age: 18,
+    location:"patna",
+    email: "nitish.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday","saturday"] 
+}
+
